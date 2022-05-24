41d2db44bdab6b86609df69b6ba9f6af560918c1
Tigranuhi <aghajanyan.tigranuhi@gmail.com>
2022-05-24 23:55:48 +0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import ReactDOM from 'react-dom/client'; 
+//import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+// const container = document.getElementById('root');
+// const root = createRoot(container);     // sa toolkit-i jamanak
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
+     {/* <Provider store={store}>
+      toolkiti hamar e
+    </Provider> */}
   </React.StrictMode>
 );
 
