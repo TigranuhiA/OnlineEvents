c7b277b0d4f6574778057299c713f58b22444260
Tatev <tzadoyan@gmail.com>
2022-05-31 22:14:30 +0400

    changes questions footer admin

--- src/components/Questions/consts.js
+++ src/components/Questions/consts.js
@@ -1,29 +1,20 @@
 const QA=[
-    {question:'harc1',
-    answer: 'pat1',
+    {question:'Top 3 Reasons To Visit Armenia',
+    answer: '1. Explore The First Christian Country In The world. 2.Take The Longest Ropeway In The World. 3.Taste The Most Ancient Cuisine In The South Caucasus ',
     id:1,
     visible: false,
     },
-    {question:'harc2',
-    answer: 'pat2',
+    {question:'Where to go in Armenia?',
+    answer: '1. Yerevan, 2. 2. Shikahogh State Reserve, 3. Lake Sevan, 4. Dilijan national park, 5. Mount Aragats',
     id:2,
     visible: false,
     },
-    {question:'harc3',
-    answer: 'pat3',
+    {question:'What are Armenia famous for?',
+    answer: 'Armenia is known for its beautiful landscapes, its cuisine, culture and history.',
     id:3,
     visible: false,
     },
-    {question:'harc4',
-    answer: 'pat4',
-    id:4,
-    visible: false,
-    },
-    {question:'harc5',
-    answer: 'pat5',
-    id:5,
-    visible: false,
-    }
+
 ]
 
 export default QA
