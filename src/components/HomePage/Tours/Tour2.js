1f49d092140ef990fc581e3d1ff8d5272a8243ab
Satik <97631974+satikmkhitaryan@users.noreply.github.com>
2022-06-03 01:40:32 +0400

    Revert "addFavorites"

--- src/components/HomePage/Tours/Tour2.js
+++ src/components/HomePage/Tours/Tour2.js
@@ -1,42 +1,4 @@
 import React from "react";
-
-import { Link  } from 'react-router-dom';
-import {tour2, price} from "../helpers/constant";
-import classes from './tour.module.css';
-import {useUserInfo} from "../../../contexts/UserProvider";
-import AddToFavorites from "../addToFavorites/AddToFavorites";
-import {useState} from "react";
-
-const Tour2=()=>{
-    const {user} = useUserInfo();
-   const [success, setSuccess]=useState(false)
-  
-
-    return (
-        <div className={classes.container}>
-          <div>
-            <h1>{tour2.title}</h1>
-             <img  className={classes.tour_item} src={tour2.t_img} alt="#" />
-            <p>{tour2.text}</p> 
-          </div>
-              
-           
-           <h3>{price}{tour2.price} ՀՀ դրամ</h3>
-               <button className={classes.btn} onClick={()=>
-                    setSuccess(true)
-            }>                   
-                ԱՎԵԼԱՑՆԵԼ ԶԱՄԲՅՈՒՂՈՒՄ</button>
-                {success && <div className={classes.fav}>
-                    <h3>Ձեր զամբյուղը</h3>
-                        <h4>{tour2.title}</h4>
-                        {tour2.text}
-                        </div>}
-             {user && <AddToFavorites price={tour2.price}/>}
-            {<div> <Link to="/login"> ԵԹԵ ՑԱՆԿԱՆՈՒՄ ԵՔ ՊԱՏՎԻՐԵԼ ՏՈՄՍ,  ՄՈՒՏՔ ԳՈՐԾԵՔ ԱՆՁՆԱԿԱՆ ԷՋ </Link> </div>  }
-            {<div> <Link to="/login"> ԵԹԵ ԴԵՌ ՉԵՔ ԳՐԱՆՑՎԵԼ, ՍԵՂՄԵՔ ԱՅՍՏԵՂ </Link> </div>}
-                      
-    
-
 import {tour2} from "../helpers/constant"
 import {price} from "../helpers/constant"
 import { useTranslate} from "../../../contexts/LanguageProvider"
@@ -50,7 +12,6 @@ const Tour2=()=>{
           <img src={tour2.tour2_img} alt="#" />
            <p>{t("tour2.text")}</p> 
            <h3>{t("price")}</h3>
-
         </div>
     )
 }
